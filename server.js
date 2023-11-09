9a5121ad88e579cf6d5cf9a18cb6c834356c4a0e
N1l228 <pubgya845@gmail.com>
2023-11-09 05:15:05 +0100

    main
    111

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const methodOverride = require('method-override');
+const Post = require('./models/post');
+const puppeteer = require('puppeteer');
 
 const app = express();
 
 app.set("view engine", "ejs");
 
 const PORT = 3000;
-const db =
-  "mongodb+srv://N1kolaenko:<N1kolaenko_228>@pasha0.jdmgxdh.mongodb.net/?retryWrites=true&w=majority";
+const db = "mongodb+srv://pubgya845:Pass321@cluster0.5zkyv6d.mongodb.net/node-blog?retryWrites=true&w=majority";
 
 mongoose
   .connect(db)
   .then((res) => console.log("Connected to DB"))
   .catch((error) => console.log(error));
 
-const createPath = (page) =>
-  path.resolve(__dirname, "ejs-views", `${page}.ejs`);
+const createPath = (page) => path.resolve(__dirname, "ejs-views", `${page}.ejs`);
 
 app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`listening port ${PORT}`);
@@ -25,13 +26,13 @@ app.listen(PORT, (error) => {
 
 app.use(express.static("photos"));
 
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms")
-);
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static("styles"));
+
+app.use(methodOverride('_method'));
 
 const { MongoClient } = require("mongodb");
 
@@ -51,39 +52,73 @@ app.get("/contacts", (req, res) => {
 });
 
 app.get("/posts/:id", (req, res) => {
-  const title = "Post";
-  const post = {
-    id: "1",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.",
-    title: "Post title",
-    date: "05.05.2021",
-    author: "Yauhen",
-  };
-  res.render(createPath("post"), { title, post });
+  const title = 'Post';
+  Post
+    .findById(req.params.id)
+    .then((post) => res.render(createPath('post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  Post
+    .findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.get('/edit/:id', (req, res) => {
+  const title = 'Edit Post';
+  Post
+    .findById(req.params.id)
+    .then(post => res.render(createPath('edit-post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.put('/edit/:id', (req, res) => {
+  const { title, author, text } = req.body;
+  const { id } = req.params;
+  Post
+    .findByIdAndUpdate(id, { title, author, text })
+    .then((result) => res.redirect(`/posts/${id}`))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
 });
 
 app.get("/posts", (req, res) => {
   const title = "Posts";
-  const posts = {
-    id: "1",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.",
-    title: "Post title",
-    date: "05.05.2021",
-    author: "Yauhen",
-  };
-  res.render(createPath("posts"), { title, posts });
+  Post
+    .find()
+    .sort({ createdAt: -1 })
+    .then((posts) => res.render(createPath('posts'), { posts, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
 });
 
 app.post("/add-post", (req, res) => {
   const { title, author, text } = req.body;
-  const post = {
-    id: new Date(),
-    date: new Date().toLocaleDateString(),
-    title,
-    author,
-    text,
-  };
-  res.render(createPath("post"), { post, title });
+  const post = new Post({ title, author, text });
+  post
+    .save()
+    .then((result) => res.redirect('/posts'))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    })
 });
 
 app.get("/add-post", (req, res) => {
